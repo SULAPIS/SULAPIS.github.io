6f9b023ef98c910d94631bfbbdb8ab99cb4169fe
SULAPIS <g2359620463@gmail.com>
2021-04-05 16:51:15 +0800

    .

--- javascript.js
+++ javascript.js
@@ -1,39 +1,36 @@
-var H='....';
-var H=H.split('');
-var M='.....';
-var M=M.split('');
-var S='......';
-var S=S.split('');
-var Ypos=0;
-var Xpos=0;
-var Ybase=8;
-var Xbase=8;
-var dots=12;
+var n = 0;
 
-function clock(){
-    var time=new Date();
-    var secs=time.getSeconds();
-    var sec=-1.57+Math.PI*secs/30;
-    var mins=time.getMinutes();
-    var min=-1.57 + Math.PI * mins/30;
-    var hr=time.getHours();
-    var hrs=-1.57 + Math.PI * hr/6 + Math.PI*parseInt(time.getMinutes())/360;
-    for (i=0; i < dots; ++i){
-        document.getElementById("dig" + (i+1)).style.top=0-15+40*Math.sin(-0.49+dots+i/1.9).toString() + "px";
-        document.getElementById("dig" + (i+1)).style.left=0-14+40*Math.cos(-0.49+dots+i/1.9).toString() + "px";
+function cc(bot) {
+    bot.value = ++n;
+}
+
+var aniframes = new Array(22);
+for (var i = 0; i < 22; i++) {
+    aniframes[i] = "img/pingu/" + i + ".jpg";
+}
+var frame = 0;
+var timeoutId = null;
+
+function animatee() {
+    document.pingu.src = aniframes[frame];
+    // alert(document.pingu.src);
+    frame = (frame + 1) % 22;
+    timeoutId = setTimeout("animatee()", 100);
+}
+
+function animatef() {
+    document.pingu.src = aniframes[frame];
+    // alert(document.pingu.src);
+    frame = frame - 1;
+    if (frame == -1) frame = 21;
+    timeoutId = setTimeout("animatef()", 100);
+}
+
+function changeImage() {
+    element = document.getElementById("bul");
+    if (element.src.match("bulbon")) {
+        element.src = "img/bulboff.gif";
+    } else {
+        element.src = "img/bulbon.gif";
     }
-    for (i=0; i < S.length; i++){
-        document.getElementById("sec" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(sec).toString() + "px";
-        document.getElementById("sec" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(sec).toString() + "px";
-    }
-    for (i=0; i < M.length; i++){
-        document.getElementById("min" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(min).toString() + "px";
-        document.getElementById("min" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(min).toString() + "px";
-    }
-    for (i=0; i < H.length; i++){
-        document.getElementById("hour" + (i+1)).style.top =Ypos+i*Ybase*Math.sin(hrs).toString() + "px";
-        document.getElementById("hour" + (i+1)).style.left=Xpos+i*Xbase*Math.cos(hrs).toString() + "px";
-    } 
-    setTimeout('clock()',50);
-    }
-    window.onload=clock;
+}
